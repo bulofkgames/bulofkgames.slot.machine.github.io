0d894d5381d6ee9fca0b396648b29af7a0494677
bulofkgames <leodiyu@gmail.com>
2025-12-24 17:48:31 -0300

    YT @BULOFK

--- js/Slot.js
+++ js/Slot.js
@@ -1,60 +1,35 @@
-import Reel from "./Reel.js";
-import Symbol from "./Symbol.js";
+import Reel from './Reel.js';
+import Symbol from './Symbol.js';
 
 export default class Slot {
   constructor(domElement, config = {}) {
     Symbol.preload();
 
-    this.currentSymbols = [
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-    ];
-
-    this.nextSymbols = [
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-    ];
-
     this.container = domElement;
 
-    this.reels = Array.from(this.container.getElementsByClassName("reel")).map(
-      (reelContainer, idx) =>
-        new Reel(reelContainer, idx, this.currentSymbols[idx])
-    );
+    this.currentSymbols = Array(5).fill(null).map(() => Array(3).fill("death_star"));
+    this.nextSymbols = Array(5).fill(null).map(() => Array(3).fill("death_star"));
+
+    this.reels = Array.from(this.container.getElementsByClassName("reel"))
+      .map((reelContainer, idx) => new Reel(reelContainer, idx, this.currentSymbols[idx]));
 
     this.spinButton = document.getElementById("spin");
     this.spinButton.addEventListener("click", () => this.spin());
 
     this.autoPlayCheckbox = document.getElementById("autoplay");
 
-    if (config.inverted) {
-      this.container.classList.add("inverted");
-    }
-
     this.config = config;
   }
 
   spin() {
     this.currentSymbols = this.nextSymbols;
-    this.nextSymbols = [
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-    ];
+    this.nextSymbols = Array(5).fill(null).map(() => Array(3).fill().map(() => Symbol.random()));
 
     this.onSpinStart(this.nextSymbols);
 
     return Promise.all(
-      this.reels.map((reel) => {
-        reel.renderSymbols(this.nextSymbols[reel.idx]);
+      this.reels.map((reel, idx) => {
+        reel.renderSymbols(this.nextSymbols[idx]);
         return reel.spin();
       })
     ).then(() => this.onSpinEnd(this.nextSymbols));
@@ -62,17 +37,15 @@ export default class Slot {
 
   onSpinStart(symbols) {
     this.spinButton.disabled = true;
-
     this.config.onSpinStart?.(symbols);
   }
 
   onSpinEnd(symbols) {
     this.spinButton.disabled = false;
-
     this.config.onSpinEnd?.(symbols);
 
     if (this.autoPlayCheckbox.checked) {
-      return window.setTimeout(() => this.spin(), 200);
+      setTimeout(() => this.spin(), 200);
     }
   }
 }
