bdfdc34a263285fdddecab9c7a82b53a2eb71da8
bulofkgames <leodiyu@gmail.com>
2025-12-24 17:47:52 -0300

    YT @BULOFK

--- js/Reel.js
+++ js/Reel.js
@@ -1,4 +1,4 @@
-import Symbol from "./Symbol.js";
+import Symbol from './Symbol.js';
 
 export default class Reel {
   constructor(reelContainer, idx, initialSymbols) {
@@ -9,45 +9,16 @@ export default class Reel {
     this.symbolContainer.classList.add("icons");
     this.reelContainer.appendChild(this.symbolContainer);
 
-    this.animation = this.symbolContainer.animate(
-      [
-        // We cannot animate translateY & filter at the same time in safari for some reasons,
-        // so we go with animating top & filter instead.
-        { top: 0, filter: "blur(0)" },
-        { filter: "blur(2px)", offset: 0.5 },
-        {
-          top: `calc((${Math.floor(this.factor) * 10} / 3) * -100% - (${
-            Math.floor(this.factor) * 10
-          } * 3px))`,
-
-          filter: "blur(0)",
-        },
-      ],
-      {
-        duration: this.factor * 1000,
-        easing: "ease-in-out",
-      }
-    );
-    this.animation.cancel();
-
-    initialSymbols.forEach((symbol) =>
+    initialSymbols.forEach(symbol => 
       this.symbolContainer.appendChild(new Symbol(symbol).img)
     );
-  }
-
-  get factor() {
-    return 1 + Math.pow(this.idx / 2, 2);
   }
 
   renderSymbols(nextSymbols) {
     const fragment = document.createDocumentFragment();
 
-    for (let i = 3; i < 3 + Math.floor(this.factor) * 10; i++) {
-      const icon = new Symbol(
-        i >= 10 * Math.floor(this.factor) - 2
-          ? nextSymbols[i - Math.floor(this.factor) * 10]
-          : undefined
-      );
+    for (let i = 0; i < nextSymbols.length; i++) {
+      const icon = new Symbol(nextSymbols[i]);
       fragment.appendChild(icon.img);
     }
 
@@ -55,24 +26,17 @@ export default class Reel {
   }
 
   spin() {
-    const animationPromise = new Promise(
-      (resolve) => (this.animation.onfinish = resolve)
-    );
-    const timeoutPromise = new Promise((resolve) =>
-      setTimeout(resolve, this.factor * 1000)
-    );
-
-    this.animation.cancel();
-    this.animation.play();
-
-    return Promise.race([animationPromise, timeoutPromise]).then(() => {
-      if (this.animation.playState !== "finished") this.animation.finish();
-
-      const max = this.symbolContainer.children.length - 3;
-
-      for (let i = 0; i < max; i++) {
-        this.symbolContainer.firstChild.remove();
-      }
+    return new Promise(resolve => {
+      this.symbolContainer.animate(
+        [
+          { transform: "translateY(0)" },
+          { transform: "translateY(-200%)" }
+        ],
+        {
+          duration: 1000 + this.idx * 200,
+          easing: "ease-in-out"
+        }
+      ).onfinish = resolve;
     });
   }
 }
