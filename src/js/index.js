9f30f621ddaab222751548e3f99236abb1661368
bulofkgames <leodiyu@gmail.com>
2025-12-24 18:30:10 -0300

    a

--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-const slot = new Slot(document.getElementById("slot"), {
-  onSpinStart: (symbols) => console.log("Spin started:", symbols),
-  onSpinEnd: (symbols) => console.log("Spin ended:", symbols),
-});
