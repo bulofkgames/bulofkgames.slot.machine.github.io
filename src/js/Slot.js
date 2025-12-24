29937f1146c07a2462ab6b695335396468effcfb
bulofkgames <leodiyu@gmail.com>
2025-12-24 18:44:54 -0300

    YT @BULOFK

--- src/js/Slot.js
+++ src/js/Slot.js
@@ -5,74 +5,31 @@ export default class Slot {
   constructor(domElement, config = {}) {
     Symbol.preload();
 
-    this.currentSymbols = [
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-    ];
-
-    this.nextSymbols = [
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-      ["death_star", "death_star", "death_star"],
-    ];
-
     this.container = domElement;
-
     this.reels = Array.from(this.container.getElementsByClassName("reel")).map(
       (reelContainer, idx) =>
-        new Reel(reelContainer, idx, this.currentSymbols[idx])
+        new Reel(reelContainer, idx, [Symbol.random(), Symbol.random(), Symbol.random()])
     );
 
     this.spinButton = document.getElementById("spin");
     this.spinButton.addEventListener("click", () => this.spin());
 
     this.autoPlayCheckbox = document.getElementById("autoplay");
-
-    if (config.inverted) {
-      this.container.classList.add("inverted");
-    }
-
     this.config = config;
   }
 
   spin() {
-    this.currentSymbols = this.nextSymbols;
-    this.nextSymbols = [
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-      [Symbol.random(), Symbol.random(), Symbol.random()],
-    ];
-
-    this.onSpinStart(this.nextSymbols);
-
-    return Promise.all(
-      this.reels.map((reel) => {
-        reel.renderSymbols(this.nextSymbols[reel.idx]);
-        return reel.spin();
-      })
-    ).then(() => this.onSpinEnd(this.nextSymbols));
-  }
-
-  onSpinStart(symbols) {
     this.spinButton.disabled = true;
 
-    this.config.onSpinStart?.(symbols);
-  }
+    const nextSymbols = this.reels.map(() => [Symbol.random(), Symbol.random(), Symbol.random()]);
+    const promises = this.reels.map((reel, idx) => {
+      reel.renderSymbols(nextSymbols[idx]);
+      return reel.spin();
+    });
 
-  onSpinEnd(symbols) {
-    this.spinButton.disabled = false;
-
-    this.config.onSpinEnd?.(symbols);
-
-    if (this.autoPlayCheckbox.checked) {
-      return window.setTimeout(() => this.spin(), 200);
-    }
+    return Promise.all(promises).then(() => {
+      this.spinButton.disabled = false;
+      if (this.autoPlayCheckbox.checked) setTimeout(() => this.spin(), 500);
+    });
   }
 }
